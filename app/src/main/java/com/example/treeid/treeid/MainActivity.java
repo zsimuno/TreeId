e548245a6cbc01dbe03684a75ab2735ab31695f1
stzbilj <s.zbiljski@gmail.com>
2018-03-01 01:28:25 +0100

    Update of csv file

--- app/src/main/java/com/example/treeid/treeid/MainActivity.java
+++ app/src/main/java/com/example/treeid/treeid/MainActivity.java
@@ -1,24 +1,14 @@
 package com.example.treeid.treeid;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.ScrollView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
 
